9834b2510795cfa8abd3d0103ae7d62522655680
Tisawesomeness <tis_dev@protonmail.com>
2022-08-13 23:13:11 -0400

    Add tab completions

--- bp-spigot/src/main/java/com/tisawesomeness/betterpreview/spigot/PreviewCommand.java
+++ bp-spigot/src/main/java/com/tisawesomeness/betterpreview/spigot/PreviewCommand.java
@@ -5,10 +5,16 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
+import org.bukkit.util.StringUtil;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 @AllArgsConstructor
-public class PreviewCommand implements CommandExecutor {
+public class PreviewCommand implements CommandExecutor, TabCompleter {
 
     private final BetterPreviewSpigot plugin;
 
@@ -71,10 +77,33 @@ public class PreviewCommand implements CommandExecutor {
             plugin.sendMessage(sender, ChatColor.RED + "You don't have permission to refresh preview for all players.");
             return;
         }
-        for (Player player : plugin.getServer().getOnlinePlayers()) {
+        for (var player : plugin.getServer().getOnlinePlayers()) {
             plugin.sendFormatter(player);
         }
         plugin.sendMessage(sender, "Refreshed all previews.");
+    }
+
+    @Override
+    public @Nullable List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        var commands = new ArrayList<String>();
+        if (args.length == 1) {
+            if (Util.hasPermission(sender, "betterpreview.refresh")) {
+                commands.add("refresh");
+            }
+            return StringUtil.copyPartialMatches(args[0], commands, new ArrayList<>());
+        }
+        if (args.length == 2 && args[0].equals("refresh") && Util.hasPermission(sender, "betterpreview.refresh")) {
+            if (Util.hasPermission(sender, "betterpreview.refresh.others")) {
+                for (var player : plugin.getServer().getOnlinePlayers()) {
+                    commands.add(player.getName());
+                }
+            }
+            if (Util.hasPermission(sender, "betterpreview.refresh.all")) {
+                commands.add("*");
+            }
+            return StringUtil.copyPartialMatches(args[1], commands, new ArrayList<>());
+        }
+        return null;
     }
 
 }
