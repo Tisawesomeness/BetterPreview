734cefcd1d9872840cf7c5a3ed98b39909db07d9
Tisawesomeness <tis_dev@protonmail.com>
2022-08-18 11:20:08 -0400

    Make sure invalid formatter packet errors

--- bp-core/src/test/java/com/tisawesomeness/betterpreview/format/FormatterRegistryTest.java
+++ bp-core/src/test/java/com/tisawesomeness/betterpreview/format/FormatterRegistryTest.java
@@ -1,9 +1,12 @@
 package com.tisawesomeness.betterpreview.format;
 
+import com.tisawesomeness.betterpreview.network.ByteBufs;
+
 import io.netty.buffer.Unpooled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class FormatterRegistryTest {
 
@@ -11,7 +14,7 @@ class FormatterRegistryTest {
     public void testWriteEmpty() {
         var buf = Unpooled.buffer();
         FormatterRegistry.write(buf, null);
-        assertThat(buf.readByte()).isEqualTo((byte) 0);
+        assertThat(ByteBufs.readVarInt(buf)).isEqualTo(0);
         assertThat(buf.isReadable()).isFalse();
     }
     @Test
@@ -25,7 +28,7 @@ class FormatterRegistryTest {
     public void testWrite() {
         var buf = Unpooled.buffer();
         FormatterRegistry.write(buf, new NopFormatter());
-        assertThat(buf.readByte()).isEqualTo((byte) 1);
+        assertThat(ByteBufs.readVarInt(buf)).isEqualTo(1);
         assertThat(buf.isReadable()).isFalse();
     }
     @Test
@@ -33,6 +36,13 @@ class FormatterRegistryTest {
         var buf = Unpooled.buffer();
         FormatterRegistry.write(buf, new NopFormatter());
         assertThat(FormatterRegistry.read(buf)).containsInstanceOf(NopFormatter.class);
+    }
+
+    @Test
+    public void testReadInvalid() {
+        var buf = Unpooled.buffer();
+        ByteBufs.writeVarInt(buf, -1);
+        assertThatThrownBy(() -> FormatterRegistry.read(buf)).isInstanceOf(IllegalArgumentException.class);
     }
 
 }
